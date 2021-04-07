0361d82b49700c7906e246ae25972377b3317598
Baranyai Bence <bence@bbence.com>
2021-04-07 21:24:33 +0200

    Add GUI

---
+++ pdf/gui/assets/libs/js/dashboard-ecommerce.js
@@ -0,0 +1,29 @@
+$("#sparkline-revenue").sparkline([5, 5, 7, 7, 9, 5, 3, 5, 2, 4, 6, 9], {
+    type: 'line',
+    width: '99.5%',
+    height: '100',
+    lineColor: '#5969ff',
+    fillColor: '#dbdeff',
+    lineWidth: 4,
+    spotColor: undefined,
+    minSpotColor: undefined,
+    maxSpotColor: undefined,
+    highlightSpotColor: undefined,
+    highlightLineColor: undefined,
+    resize: true
+});
+
+$("#sparkline-revenue2").sparkline([3, 7, 6, 4, 5, 4, 3, 5, 5, 2, 3, 1], {
+    type: 'line',
+    width: '99.5%',
+    height: '100',
+    lineColor: '#ff407b',
+    fillColor: '#ffdbe6',
+    lineWidth: 4,
+    spotColor: undefined,
+    minSpotColor: undefined,
+    maxSpotColor: undefined,
+    highlightSpotColor: undefined,
+    highlightLineColor: undefined,
+    resize: true
+});
